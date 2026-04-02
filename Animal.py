766ffe053d7524d0aed43be89c3e86bb8e3ba414
antoinej-sketch <antoinej@oregonstate.edu>
2026-04-02 00:55:26 -0400

    Add files via upload
    Project 1

---
+++ Animal.py
@@ -0,0 +1,7 @@
+# GitHub username: antoinej-sketch
+# Date: 04/01/2026
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
