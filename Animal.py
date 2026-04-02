f18cfc0cfb2b420a14e7b75ee13cb87981600175
antoinej-sketch <antoinej@oregonstate.edu>
2026-04-02 01:13:27 -0400

    Delete Animal.py

--- Animal.py
+++
@@ -1,7 +0,0 @@
-# GitHub username: antoinej-sketch
-# Date: 04/01/2026
-# Description: Asks the user for their favorite animal and then
-#              prints out that that is their favorite animal
-
-fav_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fav_animal + ".")
