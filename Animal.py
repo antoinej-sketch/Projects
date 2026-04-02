d804d9fbcdf5438b4a79ba58b8465474f29ba117
antoinej-sketch <antoinej@oregonstate.edu>
2026-04-02 01:07:35 -0400

    Fix variable name typo in Animal.py

--- Animal.py
+++ Animal.py
@@ -3,5 +3,5 @@
 # Description: Asks the user for their favorite animal and then
 #              prints out that that is their favorite animal
 
-fave_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fave_animal + ".")
+fav_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fav_animal + ".")
